42ba42dac9e70a0229af40087622e2e9589b93de
Seshrut <seshrutjain@gmail.com>
2024-10-28 20:44:31 +0530

    add frontend start backhand

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const { app, BrowserWindow,nativeTheme } = require('electron')
-
+const { app, BrowserWindow,ipcMain } = require('electron')
+const api = "removed" // remove api
 const createWindow = () => {
     const win = new BrowserWindow({
       width: 800,
@@ -16,3 +16,51 @@ const createWindow = () => {
   app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
   })
+
+ipcMain.on('promt',(evt,msg)=>{
+// todo
+})
+
+function AI_preload(){
+  // import
+  const { GoogleGenerativeAI, HarmBlockThreshold, HarmCategory } = require("@google/generative-ai");
+  // provide api key
+  const genAI = new GoogleGenerativeAI(api);
+
+    const safetySettings = [
+        {
+            category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+            threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+        },
+        {
+            category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+            threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+        },
+        {
+            catagory: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+            threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+        },
+        {
+            category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+            threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+        },
+    ];
+    // use the model
+    const gen_model = genAI.getGenerativeModel({ model: "gemini-pro" }, safetySettings);
+    // tweak model (TODO)
+
+}
+
+// function to talk to model
+async function talk(promt) {
+  const msg = promt;
+  try {
+      const result = await ichat.sendMessage(msg);
+      const response = await result.response;
+      const text = response.text();
+      return text;
+  }
+  catch (error) {
+      console.log(error)
+  }
+}
---
+++ renderer.js
@@ -0,0 +1,20 @@
+const {ipcRenderer} = require('electron');
+const button = document.getElementById('send');
+// send promt to AI
+button.addEventListener('click',()=> {
+    // promt to send
+    const text = document.getElementById('inp');
+    // send promt to send to ai
+    ipcRenderer.send('promt',text);
+
+})
+// Recieve file updates
+ipcRenderer.on('file',(evt,msg)=>{
+
+})
+
+// Recieve status updates
+ipcRenderer.on('status',(evt, msg)=>{
+    
+
+})
